a42456391a9b05f17c9504bc6dd898e2c20de03b
Karthikeyan VK <karthik_3030@gmail.com>
2024-02-12 19:43:11 +0530

    fixed the mongoscaling with private connection string

--- helper/mongo.go
+++ helper/mongo.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"os"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -11,10 +10,9 @@ import (
 // Create MongoDB client
 func makeMongoClient() (*mongo.Client, error) {
 
-
 	// change the below line to your MongoDB connection string with username and password of kubernetes
 
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	clientOptions := options.Client().ApplyURI("mongodb://admin:admin123@mongo.default.svc.cluster.local:27017/admin")
 
 	// Connect to MongoDB
 	client, err := mongo.Connect(context.Background(), clientOptions)
@@ -49,7 +47,7 @@ func Insert() error {
 	}
 	_, err = collection.InsertMany(context.Background(), docs)
 	return err
-	
+
 }
 
 // Delete all documents from MongoDB collection
